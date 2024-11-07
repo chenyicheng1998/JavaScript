ae07af4a79d64399686bb60266f20afbbce22cb6
chenyicheng1998 <chenyicheng123@sina.cn>
2024-11-07 14:15:43 +0200

    modify m1t3 js file

--- Module1/task3/t3.js
+++ Module1/task3/t3.js
@@ -5,5 +5,12 @@ const second = parseInt(prompt('Type second integer:'));
 const third = parseInt(prompt('Type third integer:'));
 
 const sum = first + second + third;
+const product = first * second * third;
+const average = sum/3;
 
-document.querySelector('#target').innerHTML = 'The sum is ' + sum;
+document.querySelector('#target').innerHTML = `
+The sum is ${sum} <br>
+The product is ${product} <br>
+The average is ${average.toFixed(2)}
+`
+;
